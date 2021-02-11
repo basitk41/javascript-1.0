e066035f546221c5d85d2696e5c67b3ba5a75a68
Basit Ali <37082492+basitk41@users.noreply.github.com>
2021-02-11 16:22:32 +0500

    Add files via upload

---
+++ lecture1.js
@@ -0,0 +1,73 @@
+// printing Hello world.(console.log + alert + write + getElementById/Tag) (browser, node).
+// console.log('Hi');
+// alert('Hi');
+// document.write('Hi');
+
+//Comments(// ,/**/).
+// signle line comment
+/*
+multi line comments
+*/
+
+// case-sensitive(let a, let A).
+// let a=10;
+// let A=20;
+// console.log(a);
+// console.log(A);
+
+//Identifiers,Statements(Start with character,camel Notation,_$,0-9),(ends with ;).
+// let firstName = 'Abbas';
+// let element = document.getElementById('root');
+// element.innerHTML = firstName;
+// element.style.backgroundColor='blue';
+// element.style.display='none';
+// console.log(element);
+
+// scope of var and let
+// let c=10;
+
+// function hi(){
+//     var a=10;
+//     console.log(a);
+//     if(true){
+//         let b=2;
+//     }
+//     console.log(b);
+
+// }
+// hi();
+
+// datatypes(number(.NaN. ), string,boolean,null,undefinded, (array,function,object)=>object ) (typeof).
+// let a = 'Abbas';
+// let b = 10;
+// let h = 10.5;
+// let c = true;
+// let d = null;
+// let f = undefined;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(h));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(f));
+
+// Expressions(2*2,2+2).
+// let a='a';
+// let b=10;
+// console.log(a/b);
+
+// let a=10;
+// let b=a;
+// a=20;
+// console.log(a);
+// console.log(b);
+
+// let array = [1,2,3];
+// let copy = [...array];
+// array.push(4);
+// console.log(array);
+// console.log(array2);
+
+// let a=20;
+// let b=30;
+// console.log(a*b);
